36f4586f5de16e1cc731a15631269eb363a73886
Nripesh Niketan <nripesh14@gmail.com>
2025-12-27 01:46:35 +0000

    Add build_debug to gitignore

--- build_debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nripeshn/Documents/PythonPrograms/chess_with_gpu/metalfish
-# Build directory: /Users/nripeshn/Documents/PythonPrograms/chess_with_gpu/metalfish/build_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MetalFishTests]=] "/Users/nripeshn/Documents/PythonPrograms/chess_with_gpu/metalfish/build_debug/metalfish_tests")
-set_tests_properties([=[MetalFishTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/nripeshn/Documents/PythonPrograms/chess_with_gpu/metalfish/CMakeLists.txt;256;add_test;/Users/nripeshn/Documents/PythonPrograms/chess_with_gpu/metalfish/CMakeLists.txt;0;")
